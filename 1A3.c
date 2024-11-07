8b68b4069751091f7e248dd302b9f796d6ab914c
SAYEDMINHAJ12 <sayedminhaj178@gmail.com>
2024-11-08 01:11:23 +0530

    C-DSA Program Files and Assignment

--- 1A3.c
+++ 1A3.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 void main(){
     int b;
-    scanf("%x")
+    printf("Enter a Hexadecimal value:\n");
+    scanf("%x",&b);
+    printf("The Decimal value is:%d\n",b);
+    printf("The Octal value is:%o\n",b);
+    printf("The Uppercase Hexadecimal value is:%5X\n",b);
 }
